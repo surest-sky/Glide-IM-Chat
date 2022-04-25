d674d6fa95a76f95d740ba8d59b7d5fd66ba27d2
chenf <chenf@surest.cn>
2022-04-25 22:09:09 +0800

    修改一下目录

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,12 @@
-import * as React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/home";
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Im from '../pages/im';
 
 const Routers = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Im />} />
+        </Routes>
+    );
 };
 export default Routers;
