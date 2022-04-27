2bb36744b4e6f971e8b30c0bb81ad78ffa74aa90
chenf <chenf@surest.cn>
2022-04-27 20:20:43 +0800

    message chat

--- src/pages/im/components/editor.tsx
+++ src/pages/im/components/editor.tsx
@@ -1,25 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Button, Textarea } from 'react-daisyui';
+import {getMessageLen} from '../../../utils/Utils'
 import '../styles/editor.scss';
-
-// https://blog.csdn.net/qq_43437571/article/details/106088271
-const getMessageLen = str => {
-    var count = 0;
-    if (str) {
-        const len = str.length;
-        for (var i = 0; i < len; i++) {
-            if (str.charCodeAt(i) > 255) {
-                count += 2;
-            } else {
-                count++;
-            }
-        }
-        console.log(count);
-        return count;
-    } else {
-        console.log(0);
-    }
-};
 
 const Editor = ({ sendMessage }) => {
     const [message, setMessage] = useState<string>('');
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -4,3 +4,22 @@ export function isMobile(): boolean {
     let smallScreen = (window.innerWidth < 600);
     return mobileAgent || smallScreen;
 }
+
+// https://blog.csdn.net/qq_43437571/article/details/106088271
+export const getMessageLen = str => {
+    var count = 0;
+    if (str) {
+        const len = str.length;
+        for (var i = 0; i < len; i++) {
+            if (str.charCodeAt(i) > 255) {
+                count += 2;
+            } else {
+                count++;
+            }
+        }
+        console.log(count);
+        return count;
+    } else {
+        console.log(0);
+    }
+};
