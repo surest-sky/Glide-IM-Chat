e9355f3a4e3ac0a48f14513ce113032e1458d5ea
chenf <chenf@surest.cn>
2022-09-22 17:48:45 +0800

    codeing

--- src/components/ChatPlay/index.js
+++ src/components/ChatPlay/index.js
@@ -11,9 +11,10 @@ const ChatPlay = () => {
     const authInfo = useSelector(state => state.container.authInfo)
     const [visible, setVisible] = useState(false)
     let host = lodash.get(authInfo, 'app.host')
-    if (!host && !isProduction) {
+    if(!host) {
         return <></>
     }
+
     // 本地客户端调试域名
     host = process.env.REACT_APP_DEBUG_HOST || "127.0.0.1:8089"
     return <div className="chat-mobile-iframe">
