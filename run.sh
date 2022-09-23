4024a0d2a7c41751d9c03ddd2d4a82b1b70b429d
chenf <chenf@surest.cn>
2022-09-23 09:37:14 +0800

    编译

--- run.sh
+++
@@ -1,2 +0,0 @@
-git pull
-git reset --hard origin/master
--- run/build.sh
+++ run/build.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
-if [ $1 == "build" ]; then
-  if [ $1 == "pc" ]; then
-    npm run build
-    tar -cJvf pc.tar.xz build
-    echo "编译完成 PC"
-    exit 1
+if [ "$1" = "build" ]
+then {
+  if [ "$2" = "pc" ]; then
+      npm run build
+      tar -cJvf pc.tar.xz build
+      echo "编译完成 PC"
+      exit 1
   fi
 
-  if [ $1 == "mobile" ]; then
-    npm run build
-    tar -cJvf mobile.tar.xz build
-    echo "编译完成 Mobile"
-    exit 1
+  if [ "$2" == "mobile" ]; then
+      npm run build:mobile
+      tar -cJvf mobile.tar.xz build
+      echo "编译完成 Mobile"
+      exit 1
   fi
 
   echo "未找到需要编译的服务"
   exit 1
+}
 fi
 
-if [ $1 == "deploy" ]; then
-  if [ $1 == "pc" ]; then
-    rm -rf ./pc
-    tar -xf pc.tar.xz
-    echo "部署成功 PC"
-    exit 1
+if [ "$1" == "deploy" ]
+then {
+  if [ "$2" == "pc" ]; then
+      rm -rf ./pc
+      mkdir -p ./build/pc && tar -xzvf pc.tar.xz -C ./run/pc --strip-components 1
+      echo "部署成功 PC"
+      exit 1
   fi
 
-  if [ $1 == "mobile" ]; then
-    rm -rf ./mobile
-    tar -xf mobile.tar.xz
-    echo "部署成功 PC"
-    exit 1
+  if [ "$2" == "mobile" ]; then
+      rm -rf ./mobile
+      mkdir -p ./build/mobile && tar -xzvf mobile.tar.xz -C ./run/mobile --strip-components 1
+      echo "部署成功 MOBILE"
+      exit 1
   fi
-
   echo "未找到需要部署的服务"
   exit 1
+}
 fi
 
+echo ">>> 未能处理成功"
